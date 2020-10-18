23bffdc9dbb2ae0b4b89a1e0eab67f7d87bb1ae3
TimTheNoodle <timthenoodle+gh@gmail.com>
2020-10-18 18:26:34 +0300

    unicafe tasks 1.6-1.11

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,78 @@
+import userEvent from '@testing-library/user-event'
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Statistics = ({ good, neutral, bad, total }) => {
+  if (total === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <table>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="total" value={total} />
+        <StatisticsLine text="average" value={(good - bad) / total} />
+        <StatisticsLine text="positive" value={good / total * 100} char={"%"}/>
+      </table>
+    </div>
+  )
+}
+
+const StatisticsLine = ({ text, value, char }) => (
+  <tr>
+    <td>{text}:</td> 
+    <td>{value}{char}</td>
+  </tr>
+)
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text="good" />
+      <Button onClick={handleNeutral} text="neutral" />
+      <Button onClick={handleBad} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
